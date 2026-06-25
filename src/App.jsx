80b8498e1ae8c33e9b2ab6f5db35a4456e377f7c
J35HN <jeshua.navaavila@gmail.com>
2026-06-24 22:25:40 -0600

    center-body not affected by background

--- src/App.jsx
+++ src/App.jsx
@@ -13,39 +13,42 @@ function App() {
 
 return (
   <main id = "homepage">
+    {/* background */}
 
-    <div className = "center-body">
-      <nav className = "links">
-        {/* Skipping every "sixth" element, to give a sense of randomness */}
-        {repeated_links.filter((_, i) => (i+1) % 6 !== 0).map((item, i) => (
-          <div key = {i}>
-            <a key = {i} href = {item.source} target = "_blank">{item.name}</a>
-          </div>
-        ))
-        }
-      </nav>
-      
-      <header>
-        <div className = "name-and-contact">
-          <div className = "name">
-            <h1> {BIO.name} </h1>
-          </div>
-          <div className = "contact">
-            <p> {BIO.email} </p>
-            <p> {BIO.phone} </p>
-            <div className = "social-media">
-              <a href = {BIO.linkedIn.url} target="_blank">
-                <img src = {BIO.linkedIn.image} alt = "LinkedIn-svg"/>
-              </a>
-              <a href = {BIO.gitHub.url} target="_blank">
-                <img src = {BIO.gitHub.image} alt = "GitHub-svg"/>
-              </a>
+    {/* Content */}
+    <div className = "screen-page">
+      <div className = "center-body">
+        <nav className = "links">
+          {/* Skipping every "sixth" element, to give a sense of randomness */}
+          {repeated_links.filter((_, i) => (i+1) % 6 !== 0).map((item, i) => (
+            <div key = {i}>
+              <a key = {i} href = {item.source} target = "_blank">{item.name}</a>
+            </div>
+          ))
+          }
+        </nav>
+        
+        <header>
+          <div className = "name-and-contact">
+            <div className = "name">
+              <h1> {BIO.name} </h1>
+            </div>
+            <div className = "contact">
+              <p> {BIO.email} </p>
+              <p> {BIO.phone} </p>
+              <div className = "social-media">
+                <a href = {BIO.linkedIn.url} target="_blank">
+                  <img src = {BIO.linkedIn.image} alt = "LinkedIn-svg"/>
+                </a>
+                <a href = {BIO.gitHub.url} target="_blank">
+                  <img src = {BIO.gitHub.image} alt = "GitHub-svg"/>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </header>
-    </div>
-
+        </header>
+      </div>
+      </div>
  </main>
   )
 }
