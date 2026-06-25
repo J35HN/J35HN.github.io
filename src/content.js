45b5e852eaf764db3b357d994790d5e483a5a92f
J35HN <jeshua.navaavila@gmail.com>
2026-06-24 18:18:59 -0600

    init of content.js file

---
+++ src/content.js
@@ -0,0 +1,10 @@
+// Imports
+
+// Navigation
+export const NAVIGATION = {
+    1: { name: "Industry", source: "#" },
+    2: { name: "Projects", source: "#" },
+    3: { name: "Volunteering", source: "#" },
+    4: { name: "Experiences", source: "#" },
+    5: { name: "Photography", source: "#" },
+};
