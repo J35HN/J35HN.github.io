ca7f4d2146eb645ab80c42cd90d965d97604a721
J35HN <jeshua.navaavila@gmail.com>
2026-06-25 22:30:05 -0600

    included colors to content.js

--- src/content.js
+++ src/content.js
@@ -1,5 +1,33 @@
 // Imports
 
+// Colors
+export const COLORS = {
+  raw_concrete: "#9e9e8e",
+  iron_oxide: "#7a3b2e",
+  acid_yellow: "#d4c422",
+  bone: "#f5f0e8",
+  asphalt: "#1a1a1a",
+  blood_red: "#b01c1c",
+  industrial_green: "#3b5e3a",
+  cobalt: "#1b3a6b",
+  tezontle: "#8c2e1a",
+  barragan_magenta: "#c0316b",
+  cenote: "#1b6bb6",
+  obsidian: "#1a1520",
+  maize: "#d4a017",
+  maguey: "#5a7a4a",
+  concreto: "#a8a090",
+  tierra_oaxaquena: "#7a4e2a",
+  oxblood: "#4a1020",
+  warm_silver: "#d8ceb8",
+  terracotta: "#b05a3a",
+  ink_blue: "#1a2440",
+  graphite: "#484848",
+  dusty_violet: "#604870",
+  saffron: "#c08820",
+  shadow_crimson: "#2a1018"
+};
+
 // Bio
 export const BIO = {
     name: "Jeshua Nava Avila",
