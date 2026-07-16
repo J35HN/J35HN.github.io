e9db55e6230608f28442552b3323d07f65bcff64
J35HN <jeshua.navaavila@gmail.com>
2026-07-16 17:52:49 -0600

    Navigator component

--- src/pages/Industry.jsx
+++ src/pages/Industry.jsx
@@ -1,20 +1,12 @@
 import '../css/Industry.css'
 import PageFrame from '../components/PageFrame'
+import Navigator from '../components/Navigator'
 import { INDUSTRY } from '../content'
-import { TONES } from '../content'
 
 function Industry() {
   return (
     <PageFrame>
-      {
-        INDUSTRY.map( (item, i) => {
-          return (
-            <div key = {i} className = {`page-frame-navigator ${TONES[ Math.floor( Math.random() * (TONES.length - 1)) ]}`}>
-              {item.title}
-            </div>
-          )
-        })
-      }
+      <Navigator PageNavContent={INDUSTRY} />
     </PageFrame>
   )
 }
